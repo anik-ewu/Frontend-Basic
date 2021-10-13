3387375547ec970b78b2d6fd37b8fec8e3c7788d
anik <sabbirhasanik@gmail.com>
2021-10-14 02:19:19 +0600

    adding ngx charts

---
+++ Angular/NGX-Charts/ngx-charts-demo/src/app/data.ts
@@ -0,0 +1,87 @@
+export var single = [
+  {
+    "name": "Gnana Prakasam",
+    "series": [
+      {
+        "name": "Jan",
+        "value": "15000"
+      },
+      {
+        "name": "Feb",
+        "value": "35000"
+      },
+      {
+        "name": "Mar",
+        "value": "45000"
+      },
+      {
+        "name": "Apr",
+        "value": "25000"
+      }
+    ],
+  },
+  {
+    "name": "Kumaresan",
+    "series": [
+      {
+        "name": "Jan",
+        "value": "20000"
+      },
+      {
+        "name": "Feb",
+        "value": "1000"
+      },
+      {
+        "name": "Mar",
+        "value": "15000"
+      },
+      {
+        "name": "Apr",
+        "value": "50000"
+      }
+    ],
+  },
+  {
+    "name": "Slack",
+    "series": [
+      {
+        "name": "Jan",
+        "value": "30000"
+      },
+      {
+        "name": "Feb",
+        "value": "10004"
+      },
+      {
+        "name": "Mar",
+        "value": "17500"
+      },
+      {
+        "name": "Apr",
+        "value": "9500"
+      }
+    ],
+  },
+  {
+    "name": "Youtube",
+    "series": [
+      {
+        "name": "Jan",
+        "value": "3000"
+      },
+      {
+        "name": "Feb",
+        "value": "10504"
+      },
+      {
+        "name": "Mar",
+        "value": "27500"
+      },
+      {
+        "name": "Apr",
+        "value": "35000"
+      }
+    ],
+  }
+
+];
