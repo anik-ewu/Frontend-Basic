c7b9a99bec8ea930c7fecad814cd60cba696e2a5
Sabbir Hasan Anik <sabbir.hasan@selise.ch>
2022-09-14 18:45:16 +0600

    learn host listen and host bind

--- Angular/test-app/src/app/custom-attirbute/hilight-directive-with-renderer/hilight-directive-with-renderer.directive.ts
+++ Angular/test-app/src/app/custom-attirbute/hilight-directive-with-renderer/hilight-directive-with-renderer.directive.ts
@@ -1,15 +1,29 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHilightDirectiveWithRenderer]'
 })
 export class HilightDirectiveWithRendererDirective implements OnInit{
 
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'green';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
+    this.backgroundColor = 'blue';
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.backgroundColor = 'red';
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'crimson');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
 
 }
